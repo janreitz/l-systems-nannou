4c60a8cf1409201ff06637ce9a3a4a862d1f34e2
Jan Reitz <jan-reitz@gmx.de>
2020-12-21 18:29:41 +0100

    Move produce function into separate file

--- src/algae.rs
+++ src/algae.rs
@@ -1,22 +1,7 @@
 use std::collections::HashMap;
 
-pub fn produce(axiom: &str, rules: &HashMap<&str, &str>) -> String {
-    let mut s = String::new();
-
-    for var in axiom.chars() {
-        match rules.get::<str>(&var.to_string()) {
-            Some(string) => {
-                // Variables are subsituted according to their production rules
-                s.push_str(string)
-            }
-            None => {
-                // Constants are simply kept 
-                s.push_str(&var.to_string())
-            }
-        }
-    };
-    s
-}
+mod l_system;
+pub use crate::l_system::produce;
 
 fn main() {
     let axiom = String::from("AB");
