5c6147b0f292efffb9349519785a81abdb3084b3
Jan Reitz <jan-reitz@gmx.de>
2020-12-21 18:09:56 +0100

    Preserve constants in the production

--- src/algae.rs
+++ src/algae.rs
@@ -6,9 +6,13 @@ pub fn produce(axiom: &str, rules: &HashMap<&str, &str>) -> String {
     for var in axiom.chars() {
         match rules.get::<str>(&var.to_string()) {
             Some(string) => {
+                // Variables are subsituted according to their production rules
                 s.push_str(string)
             }
-            None => println!("This appears to be a constant"),
+            None => {
+                // Constants are simply kept 
+                s.push_str(&var.to_string())
+            }
         }
     };
     s
